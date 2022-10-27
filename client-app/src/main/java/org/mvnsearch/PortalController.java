944d4bc6c46b269a41a5bde573c94c3968ab9358
linux_china <libing.chen@gmail.com>
2022-10-27 11:05:15 +0800

    app: PortalController for rest api

---
+++ client-app/src/main/java/org/mvnsearch/PortalController.java
@@ -0,0 +1,49 @@
+package org.mvnsearch;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+public class PortalController {
+    private final ReactiveDiscoveryClient discoveryClient;
+    private final WebClient.Builder loadBalancedWebClientBuilder;
+
+    public PortalController(ReactiveDiscoveryClient discoveryClient,
+                            WebClient.Builder webClientBuilder) {
+        this.discoveryClient = discoveryClient;
+        this.loadBalancedWebClientBuilder = webClientBuilder;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello";
+    }
+
+    @GetMapping("/instances/{name}")
+    public Mono<List<ServiceInstance>> instances(@PathVariable("name") String name) {
+        return discoveryClient.getInstances(name).collectList();
+    }
+
+    @GetMapping("/services")
+    public Flux<String> services() {
+        return discoveryClient.getServices();
+    }
+
+    @GetMapping("/server-call")
+    public Mono<String> callServer() {
+        return loadBalancedWebClientBuilder.build()
+                .get()
+                .uri("http://server-app/")
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(s -> "Response from server-app: " + s);
+    }
+}
