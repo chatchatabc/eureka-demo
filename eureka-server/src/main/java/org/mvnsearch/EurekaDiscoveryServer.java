73a55521e5479fc2bcec4e8b46a4839b628816b8
linux_china <libing.chen@gmail.com>
2022-10-27 11:08:01 +0800

    app: Eureka Server

---
+++ eureka-server/src/main/java/org/mvnsearch/EurekaDiscoveryServer.java
@@ -0,0 +1,14 @@
+package org.mvnsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaDiscoveryServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDiscoveryServer.class, args);
+    }
+}
